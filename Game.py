0d8dcd910e6e792d51c76a043e25078b958c3a4c
jjgoings <jjgoings@gmail.com>
2015-10-09 19:06:08 -0700

    added Dice and Hand classes

---
+++ Game.py
@@ -0,0 +1,36 @@
+from random import randint
+
+class Dice():
+
+    def __init__(self,num_faces):
+        self.num_faces = num_faces
+        self.number = randint(1,self.num_faces)
+
+    def roll(self):
+        self.number = randint(1,self.num_faces)
+        return self.number
+
+    def print_number(self):
+        print self.number
+
+
+class Hand():
+
+    def __init__(self,num_die=5,num_faces=6):
+        self.num_die = num_die
+        self.this_hand = []
+        for die in xrange(self.num_die):
+            self.this_hand.append(Dice(num_faces))
+
+    def print_hand(self):
+        tot = 0.0
+        for die in xrange(self.num_die):
+            self.this_hand[die].print_number()
+            tot += self.this_hand[die].number
+        print "Tot %4d" % tot
+        
+        
+if __name__ == '__main__':
+    my_hand = Hand(5)
+    my_hand.print_hand()
+
---
+++ yahtzee.py
@@ -0,0 +1,15 @@
+from Game import *
+import sys
+
+def main():
+    """Main entry point."""
+    game = Game()
+    print game.score
+
+
+def function():
+    """Do a function!."""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
