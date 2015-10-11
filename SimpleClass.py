f60ae1ede794cc3fc986196d8042ec7488e00a5f
jjgoings <jjgoings@gmail.com>
2015-10-11 15:57:45 -0700

    SimpleClass to optimize simple dice roll game

---
+++ SimpleClass.py
@@ -0,0 +1,95 @@
+from __future__ import division
+import numpy as np
+from numpy.random import randint
+
+"""
+    This is a class that sets up the following game:
+
+       Say you have a die with a certain number of faces, and a 
+       finite number of rolls. You receive the score of the die.
+       You may continue rolling, or you may take your score as is. 
+       This class finds the optimal roll strategy (either roll or
+       hold) for the finite numer of rolls for the given die. 
+
+       You may change the number of rolls or the number of faces
+       on the die.
+
+       In __main__ you can hard code the number of games following 
+       this strategy. In the limit of large number of games, you 
+       should get the optimal expected value. Some optimal strategy
+       expected values are:
+
+       num_faces = 6 & num_rolls = 1 : expected score = 3.5
+       num_faces = 6 & num_rolls = 2 : expected score = 4.25
+       num_faces = 6 & num_rolls = 3 : expected score = 4.66
+
+"""
+
+
+class Game:
+
+
+    def __init__(self,num_faces=6,num_turns=3):
+        self.num_faces          = num_faces
+        # reward vector is just the number on each face, but of course
+        # it needn't be!
+        # Can I make init of reward vector more elegant?
+        self.reward_vector      = np.empty((self.num_faces,1))
+        self.reward_vector[:,0] = np.arange(1,self.num_faces+1)
+        # probability to roll another face
+        self.trans_roll         = np.ones((self.num_faces,self.num_faces)) \
+                                    * (1.0/self.num_faces)
+        # probability to stay the same -- identity matrix!
+        self.trans_hold         = np.eye(self.num_faces)
+        # state_vector not in use at the moment, I'm keeping it
+        # for when I generalize this routine
+        self.state_vector       = np.zeros((self.num_faces,1))
+        self.num_turns          = num_turns
+        self.num_turns_left     = num_turns
+
+    def expectedValues(self): 
+        # NVec updates reward vector based on number of rolls
+        # if we only rolled once, NVec would remain reward vector
+        NVec = np.copy(self.reward_vector)
+        for turn in xrange(self.num_turns_left):
+            expected_value_Roll = np.dot(self.trans_roll,NVec)
+            expected_value_Hold = np.dot(self.trans_hold,NVec)
+            for i in xrange(self.num_faces):
+                NVec[i] = max(expected_value_Hold[i],\
+                              expected_value_Roll[i])
+        return np.mean(NVec) 
+
+    def play_game(self):
+        for roll in xrange(self.num_turns):
+            # since num_turns_left = num_turns, decrement by 1
+            # at the top because this is our first roll
+            self.num_turns_left -= 1
+            if roll == 0:
+            # no action on first roll...just roll!
+                current_score = randint(1,self.num_faces+1)
+            else:
+                exp_roll = self.expectedValues()
+                if exp_roll > current_score:
+                    current_score = randint(1,self.num_faces+1)
+        # reset num_turns_left
+        self.num_turns_left = self.num_turns
+        return current_score
+         
+               
+if __name__ == '__main__':
+    num_games = 1000
+    score = 0.0
+    # Game(num_faces,num_rolls)
+    my_game = Game(6,3)
+    for game in range(num_games):
+        current_score = my_game.play_game() 
+        score += current_score
+    
+    avg_score = score/num_games    
+    
+    print avg_score
+    
+    
+
+
+
