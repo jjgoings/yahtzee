e0a02d7e1877dba2abf10196917dd95a98c9f08c
jjgoings <jjgoings@gmail.com>
2015-10-21 19:07:02 -0700

    test.py to brute force probabilities of rolls

---
+++ test.py
@@ -0,0 +1,26 @@
+import numpy as np
+import collections
+import itertools
+
+three_combos = (list(itertools.product('123456',repeat=3)))
+
+hands = []
+for combo in three_combos:
+    hand = np.zeros((6,1))
+    for i in xrange(len(combo)):
+        hand[int(combo[i])-1,0] += 1
+    non_zeros = np.nonzero(hand)
+#    print hand
+#    print non_zeros
+    if len(non_zeros[0]) == 1:
+       hands.append('3')
+    elif len(non_zeros[0]) == 2:
+       hands.append('12')
+    elif len(non_zeros[0]) == 3:
+       hands.append('111')
+
+counter = collections.Counter(hands)
+
+print counter.values()
+print counter.keys()
+
